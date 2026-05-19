f8d186e4d5da2b30fcc84f171d1d2299d54301f8
gpeppas <gpeppas@ajpsoft.gr>
2026-05-19 19:52:33 +0300

    feat: add a new addition method

---
+++ src/gr/aueb/cf/Add.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf;
+
+public class Add {
+    static void main() {
+        System.out.println(add(3,5));
+    }
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+}
