202573f05547ea5199d0172bea73b8ffd78846ff
gpeppas <gpeppas@ajpsoft.gr>
2026-05-24 20:03:18 +0300

    doublepush

--- src/gr/aueb/cf/UserMain.java
+++ src/gr/aueb/cf/UserMain.java
@@ -1,4 +1,15 @@
 package gr.aueb.cf;
 
 public class UserMain {
+    static void main() {
+        User player = new User(1, "Lebron", "James");
+
+        player.setId(1);
+        player.setFirstname("Lebron");
+        player.setLastname("James");
+
+        System.out.println("User id" + player.getId());
+        System.out.println("User firstname: " + player.getFirstname());
+        System.out.println("User lastname: " + player.getLastname());
+    }
 }
